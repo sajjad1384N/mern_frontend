b3210c056a8ff9e64dac07ab0cec2b898189b160
md sajjad ansari <sajjadansari1384@gmail.com>
2024-06-15 14:31:42 -1000

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+import Homepage from './pages/Homepage';
+import ChatPage from './pages/ChatPage';
+
+import { Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+      <Route path='/' element={<Homepage/>}/>
+      <Route path='/chats' element={<ChatPage/>}/>
+      </Routes>
+      
+    
+    </div>
+  );
+}
+
+export default App;
